27cfb70ce234d5c44b2bfe5ccad2ef034b2d939f
Victor <github.php1995@gmail.com>
2021-01-17 15:03:34 +0300

    Initial commit

---
+++ src/test/java/com/java1995/spring_crud/SpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java1995.spring_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
