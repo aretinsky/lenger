5ef5eba2ef7bb5fb4c31c54dac01929ec0274877
Victor Aretinsky <victor.aretinsky@gmail.com>
2021-02-01 16:25:20 +0300

    create laravel project

---
+++ resources/templates/index.php
@@ -0,0 +1,238 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Шаблоны сайтов</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <link rel="stylesheet" href="../static/css/style.css">
+</head>
+<body>
+<div class="wrapper">
+    <div class="header">
+        <div class="headerContent">
+            <div class="left">
+                <h1><a href="#">Aretinsky academy</a></h1>
+                <p>Уроки программирования на языке PHP</p></div>
+            <div class="right">
+                <form class="search">
+                    <input type="text" placeholder="Найти...">
+                    <input type="image" src="../static/images/search.png" title="Поиск">
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="slider">
+        <div class="itemSlider">
+            <div class="bgSlide"><img src="../static/images/code.jpg"></div>
+            <div class="descSlide">
+                <h1>Научитесь программировать</h1>
+                <p></p>
+                <span></span>
+                <p>Через <span id="days">90</span> дней вы освоите азы программирования, создав собственный веб-сайт</p>
+            </div>
+        </div>
+    </div>
+    <div class="nav">
+        <ul class="menu">
+            <li><a href="#">О курсе</a></li>
+            <li><a href="#">Вопросы и ответы</a></li>
+            <li><a href="#">О проекте</a></li>
+        </ul>
+    </div>
+    <div class="main">
+        <div class="leftCol">
+            <form>
+                <h2>Регистрация</h2>
+                <input type="text" placeholder="Как к вам обращаться?">
+                <input type="text" placeholder="Ваш email">
+                <input type="text" placeholder="Пароль">
+                <h5 id="age">Сколько вам лет?</h5>
+                <select>
+                    <option>12-14</option>
+                    <option>15-20</option>
+                    <option>20-25</option>
+                    <option>26-35</option>
+                    <option>36-45</option>
+                    <option>46+</option>
+                    <option>Секрет</option>
+                </select>
+                <div class="row">
+                    <div class="clear"></div>
+                    <input type="checkbox">
+                    <label>Я буду программистом</label>
+                    <div class="clear"></div>
+                </div>
+                <button type="submit" class="minWidth">Начать обучение</button>
+            </form>
+
+        </div>
+        <div class="rightCol">
+            <h1>Программа_обучения</h1>
+            <p>1-2 Неделя: вы будете изучать основы языка программирования на PHP.</p>
+            <p>Первые две недели вы будете изучать основы языка программирования на PHP.</p>
+            <p>Первые две недели вы будете изучать основы языка программирования на PHP.</p>
+            <p>Первые две недели вы будете изучать основы языка программирования на PHP.</p>
+            <ul class="img">
+                <li><a href="#"><img src="../static/images/img1.png"></a></li>
+                <li><a href="#"><img src="../static/images/img2.png"></a></li>
+                <li><a href="#"><img src="../static/images/img3.png"></a></li>
+            </ul>
+            <h1>Lists</h1>
+            <div class="row">
+                <div class="span">
+                    <h4>List style type circle <code>&lt;ul&gt;</code></h4>
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet</li>
+                        <li>Sed ut perspiciatis unde</li>
+                        <li>At vero eos et accusamus
+                            <ul>
+                                <li>Lorem ipsum dolor sit amet</li>
+                                <li>Sed ut perspiciatis unde</li>
+                                <li>At vero eos et accusamus</li>
+                            </ul>
+                        </li>
+                        <li>Lorem ipsum dolor sit amet</li>
+                        <li>Sed ut perspiciatis unde</li>
+                        <li>At vero eos et accusamus</li>
+                    </ul>
+                </div>
+                <div class="span">
+                    <h4>Unstyled <code>&lt;ul class="unstyled"&gt;</code></h4>
+                    <ul class="unstyled">
+                        <li>Lorem ipsum dolor sit amet</li>
+                        <li>Sed ut perspiciatis unde</li>
+                        <li>At vero eos et accusamus
+                            <ul>
+                                <li>Lorem ipsum dolor sit amet</li>
+                                <li>Sed ut perspiciatis unde</li>
+                                <li>At vero eos et accusamus</li>
+                            </ul>
+                        </li>
+                        <li>Lorem ipsum dolor sit amet</li>
+                        <li>Sed ut perspiciatis unde</li>
+                        <li>At vero eos et accusamus</li>
+                    </ul>
+                </div>
+                <div class="span">
+                    <h4>Ordered <code>&lt;ol&gt;</code></h4>
+                    <ol>
+                        <li>Lorem ipsum dolor sit amet</li>
+                        <li>Sed ut perspiciatis unde</li>
+                        <li>At vero eos et accusamus</li>
+                        <li>Lorem ipsum dolor sit amet</li>
+                        <li>Sed ut perspiciatis unde</li>
+                        <li>At vero eos et accusamus</li>
+                    </ol>
+                </div>
+            </div>
+            <h1>Tables</h1>
+            <table>
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Lorem</th>
+                    <th>Ipsum</th>
+                    <th>Dolor</th>
+                    <th>Sit</th>
+                    <th>Amet</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Lorem ipsum</td>
+                    <td>At vero eos</td>
+                    <td>Sed ut</td>
+                    <td>Dolor sit amet</td>
+                    <td><a href="#">Epd.</a></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Sed ut</td>
+                    <td>Dolor sit amet</td>
+                    <td>Lorem ipsum</td>
+                    <td>At vero eos</td>
+                    <td><a href="#">Epd.</a></td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>At vero eos</td>
+                    <td>Lorem ipsum</td>
+                    <td>Sed ut</td>
+                    <td>Dolor sit amet</td>
+                    <td><a href="#">Epd.</a></td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Dolor sit amet</td>
+                    <td>At vero eos</td>
+                    <td>Lorem ipsum</td>
+                    <td>Sed ut</td>
+                    <td><a href="#">Epd.</a></td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>At vero eos</td>
+                    <td>Lorem ipsum</td>
+                    <td>Sed ut</td>
+                    <td>Dolor sit amet</td>
+                    <td><a href="#">Epd.</a></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="footerContent">
+            <div class="span1">
+                <h1>Sed ut</h1>
+                <div class="borderBottom"></div>
+                <p>Lorem ipsum dolor sit</p>
+                <ul class="clock">
+                    <li>Nights! Absolutely No Extra Charge</li>
+                    <li>Weekends! Absolutely No Extra Charge</li>
+                    <li>Holidays! Absolutely No Extra Charge</li>
+                </ul>
+                <div class="social">
+                    <div>Мы в социальных сетях:</div>
+                    <ul>
+                        <li><a target="newtab" href="http://www.facebook.com/?sk=app_2309869772"><img
+                                        src="../static/images/facebook.png"></a></li>
+                        <li><a target="newtab" href="https://twitter.com/psdhtmlcss"><img
+                                        src="../static/images/twitter.png"></a>
+                        </li>
+                        <li><img src="../static/images/vk.png"></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="span1">
+                <h1>At vero eos</h1>
+                <div class="borderBottom"></div>
+                <p>Lorem ipsum dolor sit</p>
+                <ul>
+                    <li>West Hollywood (323) 221-1107</li>
+                    <li>Beverly Hills (310) 202-5428</li>
+                    <li>Pasadena (626) 296-2664</li>
+                    <li>West Hollywood (323) 221-1107</li>
+                    <li>Beverly Hills (310) 202-5428</li>
+                </ul>
+                <p><strong>Lorem ipsum dolor sit amet</strong></p>
+            </div>
+            <div class="span1">
+                <h1>Lorem ipsum dolor</h1>
+                <div class="borderBottom"></div>
+                <p>Lorem ipsum dolor sit</p>
+                <ul class="unstyled">
+                    <li>Связаться с 'Aretinsky':</li>
+                    <li><a href="mailto:psdhtmlcss@mail.ru">aretinsky@gmail.com</a></li>
+                    <li>5104 W. Washington Blvd</li>
+                    <li>Московская обл., поселок Серебряные Пруды</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
